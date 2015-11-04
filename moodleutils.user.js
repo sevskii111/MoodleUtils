57384f27a0328d64e39bd69ccb103bd4a52077b8
Willsr71 <Willsr71@users.noreply.github.com>
2015-11-04 08:36:13 -0500

    Update and rename autofill.user.js to moodleutils.user.js

---
+++ moodleutils.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Moodle Utils
+// @namespace    http://willsr71.net
+// @version      0.1
+// @description  Provides a number of cheats for Moodle
+// @author       Willsr71
+// @updateURL    https://github.com/Willsr71/MoodleUtils/raw/master/moodleutils.user.js
+// @match        http://www.latintutorial.com/moodle/*
+// ==/UserScript==
+
+var developmentMode = false;
+var logVisible = true;
+var script = document.createElement('script');
+
+if (developmentMode) {
+	script.src = document.location.protocol + "//willsr71.net/scripts/moodle/moodleutils.js?t=" + Date.now();
+} else {
+	script.src = document.location.protocol + "//https://github.com/Willsr71/MoodleUtils/raw/master/moodleutils.js";
+}
+
+(document.body || document.head || document.documentElement).appendChild(script);
