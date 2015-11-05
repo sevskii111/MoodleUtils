3713bf021cb836fd6071b4b3d16ebd8d60eaec96
Willsr71 <Willsr71@users.noreply.github.com>
2015-11-05 11:32:23 -0500

    Added configuration options

--- moodleutils.user.js
+++ moodleutils.user.js
@@ -1,20 +1,27 @@
 // ==UserScript==
 // @name         Moodle Utils
 // @namespace    http://willsr71.net
-// @version      0.1
+// @version      0.2
 // @description  Provides a number of cheats for Moodle
 // @author       Willsr71
 // @updateURL    https://github.com/Willsr71/MoodleUtils/raw/master/moodleutils.user.js
 // @match        http://www.latintutorial.com/moodle/*
-// @grant        none
+// @grant        localstorage
 // @run-at       document-body
 // ==/UserScript==
 
-var developmentMode = false;
-var logVisible = true;
+var configuration = {
+	devMode: false,
+	showPanel: true,
+	showConsole: false,
+	showButtons: true
+};
+
+window.localStorage.setItem("moodleutils_config", JSON.stringify(configuration));
+
 var script = document.createElement('script');
 
-if (developmentMode) {
+if (configuration.devMode) {
 	script.src = document.location.protocol + "//willsr71.net/scripts/moodle/moodleutils.js?t=" + Date.now();
 } else {
 	script.src = document.location.protocol + "//willsr71.net/scripts/moodle/moodleutils.min.js";
