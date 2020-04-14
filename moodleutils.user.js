588be6c7e5ea01f7bcc28a0e13cc705487f45d26
sevskii <sevskii111@gmail.com>
2020-04-14 17:37:26 +0300

    more novsu update

--- moodleutils.user.js
+++ moodleutils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Moodle Utils
 // @namespace    http://willsr71.net
-// @version      0.4
+// @version      0.5
 // @description  Provides a number of cheats for Moodle
 // @author       Willsr71
 // @updateURL    https://github.com/sevskii111/MoodleUtils/raw/master/moodleutils.user.js
@@ -22,10 +22,6 @@ window.localStorage.setItem("moodleutils_config", JSON.stringify(configuration))
 
 var script = document.createElement('script');
 
-if (configuration.devMode) {
-	script.src = document.location.protocol + "//willsr71.net/scripts/moodle/moodleutils.js?t=" + Date.now();
-} else {
-	script.src = document.location.protocol + "//willsr71.net/scripts/moodle/moodleutils.min.js";
-}
+script.src = "https://raw.githubusercontent.com/sevskii111/MoodleUtils/master/moodleutils.js";
 
 (document.body || document.head || document.documentElement).appendChild(script);
